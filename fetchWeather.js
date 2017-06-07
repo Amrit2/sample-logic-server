698eb2625792db8e0d2664d6d84504b587d8ac2f
Amrit <amrit.kaur2@hotmail.com>
2017-06-07 12:23:21 +1200

    API key

--- fetchWeather.js
+++ fetchWeather.js
@@ -3,7 +3,7 @@ const axios = require('axios')
     module.exports = function fetchWeather(locationName) {
       // Be sure to replace this with the API key from your openweathermap account
       // https://home.openweathermap.org/api_keys
-      const appId = '<YOUR_OPENWEATHER_APP_ID>'
+      const appId = '<539c44d823e1f08df56e6fe4abc969a1>'
       const requestUrl = `http://api.openweathermap.org/data/2.5/weather?units=imperial&appid=${appId}&q=${locationName}`
 
       return axios.get(requestUrl)
